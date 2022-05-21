b642dc8e46012773e3adef27e648195e717074d5
Whatsapp_Bot <87086889+Valenciahack@users.noreply.github.com>
2022-05-21 03:03:31 +0200

    upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -181,7 +181,7 @@ var teks = `*𝙷𝙾𝙻𝙰* *${v.pushName}* *𝙰𝚀𝚄𝙸́ 𝙴𝚂𝚃�
 
 \t\t\t\t\t\t\t\t\t *INFO USER*
 
-° ඬ⃟ Nombre: *${v.pushName}*
+│ ➼ Nombre: *${v.pushName}*
 │ ➼ Bio: *${bio}*
 │ ➼ Rango: *${rank}*
 │ ➼ Balance: *$${bal}*
@@ -189,7 +189,7 @@ var teks = `*𝙷𝙾𝙻𝙰* *${v.pushName}* *𝙰𝚀𝚄𝙸́ 𝙴𝚂𝚃�
 \t\t\t\t\t\t\t\t\t *COMANDOS*
 
 »  *𝐕𝐈𝐏*
-➼ ${prefix}join <link>${!inky.isJadi ? `
+° ඬ⃟  ${prefix}join <link>${!inky.isJadi ? `
 ➼ ${prefix}serbot` : ''}
 
 \t●Ⓖⓡⓤⓟⓞⓢ●
