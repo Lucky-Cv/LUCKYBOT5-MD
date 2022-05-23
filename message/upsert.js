964486402dd8a48a912aaed0480ba068efacf1bd
Whatsapp_Bot <87086889+Valenciahack@users.noreply.github.com>
2022-05-23 11:40:27 +0200

    update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -349,9 +349,9 @@ case 'ban':
 					if (!v.isGroup) return v.reply(mess.only.group)
 					if (!isGroupAdmins) return v.reply(mess.only.admins)	
 				if (!isBotAdmin) return v.reply(mess.only.badmin)	
-					if (v.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return reply('Etiqueta un mensaje oh utiliza @!')
-				    if (mek.message.extendedTextMessage === undefined || mek.message.extendedTextMessage === null) return reply('Etiqueta un mensaje oh utiliza @!')
-			bai = mek.message.extendedTextMessage.contextInfo.participant
+					if (v.message.extendedTextMessage === undefined || v.message.extendedTextMessage === null) return reply('Etiqueta un mensaje oh utiliza @!')
+				    if (!v.message.extendedTextMessage === undefined || v.message.extendedTextMessage === null) return reply('Etiqueta un mensaje oh utiliza @!')
+			bai = v.message.extendedTextMessage.contextInfo.participant
 		    cnf.groupRemove(from, [bai])
 					break
 
