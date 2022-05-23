fb5db3c4cfecf4ad263820cd461565f238f3efc9
Whatsapp_Bot <87086889+Valenciahack@users.noreply.github.com>
2022-05-23 12:39:09 +0200

    update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -119,8 +119,8 @@ module.exports = async(inky, v, store) => {
 				await fs.unlinkSync(nameMp4 + '.mp4')
 			}
 		}
-		if (isAntiLink && isBotAdmin && !isGroupAdmins && v.body.includes('chat.whatsapp.com/')) {
-			if (v.body.split('chat.whatsapp.com/')[1].split(' ')[0] === (await inky.groupInviteCode(v.chat))) return
+		if (isAntiLink && isBotAdmin && !isGroupAdmins && v.body.includes('http')) {
+			if (v.body.split('http')[1].split(' ')[0] === (await inky.groupInviteCode(v.chat))) return
 			inky.groupParticipantsUpdate(v.chat, [v.sender], 'remove')
 				.then(x => v.reply('@' + senderNumber + ' ha sido eliminado por mandar link de otro grupo'))
 				.catch(e => v.reply(e))
